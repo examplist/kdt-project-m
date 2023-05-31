fee2e5e102bd39c960c4fa8c6889fcbaa5608814
examplist <iykl3670@gmail.com>
2023-05-31 10:34:14 +0900

    첫 커밋

---
+++ kdt-project-m/src/main/java/com/example/demo/KdtProjectMApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtProjectMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtProjectMApplication.class, args);
+	}
+
+}
